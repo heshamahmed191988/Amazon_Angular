6c4e6140faaa33bf8d049f5cdb067e59ae8b0859
7asnaam0 <hasnaasaif50@gmail.com>
2024-03-26 11:52:16 +0200

    hasnaa pull

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent{
+
 
 }
+
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     baseUrl:"http://localhost:9793"
+    
 
 };
